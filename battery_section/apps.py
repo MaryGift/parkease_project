0328fbcea567a76fe1b3780839480a6f1b459a0e
MaryGift <marygift7066@gmail.com>
2026-04-21 07:53:36 +0300

    New Parkease project with apps created

---
+++ battery_section/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BatterySectionConfig(AppConfig):
+    name = 'battery_section'
---
+++ tyre_clinic/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TyreClinicConfig(AppConfig):
+    name = 'tyre_clinic'
